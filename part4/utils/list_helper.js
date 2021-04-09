adf7bb7f1367f663bd3ae53b53a1d403e603b2fe
Alejandro Bravo de la Serna <thezombor@gmail.com>
2021-04-09 21:54:03 +0300

    4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -3,6 +3,15 @@ const dummy = (blog) => {
   return 1
 }
 
+const totalLikes = (blog) => {
+  let total = 0
+  blog
+    .forEach(element => {
+      total += element.likes
+    })
+  return total
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
