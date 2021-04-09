ab6ca3c4ec8a32ed9330c17738443ba545bbe5b2
Alejandro Bravo de la Serna <thezombor@gmail.com>
2021-04-10 02:48:50 +0300

    4.10 and 4.11

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -34,6 +34,46 @@ test('blog have the attribute id', async () => {
   expect(response.body[0].id).toBeDefined()
 })
 
+test('post to /api/blogs working correctly', async () => {
+  const newBlog = {
+    title: 'Blog 3',
+    author: 'Author 3',
+    url: 'http://blogs.com/3',
+    likes: 10
+  }
+
+  await api
+    .post('/api/blog')
+    .send(newBlog)
+    .expect(201)
+
+  const response = await api.get('/api/blog')
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+  const newBlogPost = response.body.pop()
+  delete newBlogPost.id
+  delete newBlogPost.__v
+  expect(newBlogPost).toEqual(newBlog)
+})
+
+test('if likes misses in the post is assigned value 0 automatically', async () => {
+  const newBlog = {
+    title: 'Blog 3',
+    author: 'Author 3',
+    url: 'http://blogs.com/3'
+  }
+
+  await api
+    .post('/api/blog')
+    .send(newBlog)
+    .expect(201)
+
+  const response = await api.get('/api/blog')
+
+  const lastBlog = response.body.pop()
+  expect(lastBlog.likes).toBe(0)
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,5 +1,3 @@
-// eslint-disable-next-line no-unused-vars
-
 const initialBlogs = [
   {
     title: 'Blog 1',
