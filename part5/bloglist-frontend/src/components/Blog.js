b6e2b99d27530b723ca229abf1ee80c37fa940f7
Alejandro Bravo de la Serna <thezombor@gmail.com>
2021-04-24 21:08:46 +0300

    5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,39 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [loginVisible, setLoginVisible] = useState(false)
+  const hideWhenVisible = { display: loginVisible ? 'none' : '' }
+  const showWhenVisible = { display: loginVisible ? '' : 'none' }
+  
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  return(
+    <div>
+      <div style={hideWhenVisible}>
+        <div style={blogStyle}>
+          {blog.title} {blog.author}
+          <button onClick={() => setLoginVisible(true)}>view</button>
+        </div>
+      </div>
+    
+    <div style={showWhenVisible}>
+      <div style={blogStyle}>
+        {blog.title} {blog.author}
+        <button onClick={() => setLoginVisible(false)}>hide</button> <br/>
+        {blog.url} <br/>
+        likes {blog.likes} 
+        <button onClick={() => null}>like</button> <br/>
+        {blog.user.name} <br/>
+      </div>
+    </div>
   </div>
-)
+  )
+}
 
 export default Blog
