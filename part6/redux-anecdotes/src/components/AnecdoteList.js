4ac7c1a0e09be4bdfa44bcc4ff731a346d919f81
Alejandro Bravo de la Serna <thezombor@gmail.com>
2021-04-26 19:49:44 +0300

    6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector  } from 'react-redux'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state.anecdotes)
   const vote = (anecdote) => {
     dispatch(addVote(anecdote))
     console.log('vote', anecdote)
