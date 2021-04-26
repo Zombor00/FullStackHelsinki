e6a5152fb94e848ac8fbd533f4624f970b028501
Alejandro Bravo de la Serna <thezombor@gmail.com>
2021-04-26 13:15:59 +0300

    Bugs fixed

--- part5/bloglist-frontend/cypress/integration/blogs_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blogs_app.spec.js
@@ -34,14 +34,13 @@ describe('Blog app', function() {
       cy.get('#login-button').click()
 
       cy.contains('Wrong username or password')
+      cy.get('html').should('not.contain', 'Alex logged-in')
     })
   })
 
   describe('When logged in', function() {
     beforeEach(function() {
-      cy.get('#username').type('alex_bravo')
-      cy.get('#password').type('patata')
-      cy.get('#login-button').click()
+      cy.login({ username: 'alex_bravo', password: 'patata' })
     })
 
     it('A blog can be created', function() {
@@ -50,26 +49,45 @@ describe('Blog app', function() {
       cy.get('#author').type('Alex')
       cy.get('#url').type('www.blog0.com')
       cy.get('#create-button').click()
-      
+
       cy.contains('Blog 0 Alex')
     })
 
     describe('When logged in and with a blog', function() {
       beforeEach(function() {
-        cy.contains('new blog').click()
-        cy.get('#title').type('Blog 0')
-        cy.get('#author').type('Alex')
-        cy.get('#url').type('www.blog0.com')
-        cy.get('#create-button').click()
+        cy.createBlog({ title: 'Blog 0', author: 'Alex', url: 'www.blog0.com', likes: 0 })
       })
 
       it('A blog can be liked', function() {
         cy.contains('view').click()
         cy.contains('likes 0')
         cy.contains('like').click()
-        
+
         cy.contains('likes 1')
       })
-    }) 
+
+      it('A blog can be deleted', function() {
+        cy.contains('Blog 0')
+        cy.contains('view').click()
+        cy.contains('remove').click()
+
+        cy.get('.notif').contains('The blog Blog 0 by Alex has been deleted')
+      })
+
+      describe('When logged in and with four blog', function() {
+        beforeEach(function() {
+          cy.createBlog({ title: 'Blog 1', author: 'Alex', url: 'www.blog1.com', likes: 5 })
+          cy.createBlog({ title: 'Blog 2', author: 'Alex', url: 'www.blog2.com', likes: 25 })
+          cy.createBlog({ title: 'Blog 3', author: 'Alex', url: 'www.blog3.com', likes: 3 })
+        })
+
+        it('Blogs are sorted by number of likes', function() {
+          cy.get('.blogFull').eq(0).contains('likes 25')
+          cy.get('.blogFull').eq(1).contains('likes 5')
+          cy.get('.blogFull').eq(2).contains('likes 3')
+          cy.get('.blogFull').eq(3).contains('likes 0')
+        })
+      })
+    })
   })
 })
